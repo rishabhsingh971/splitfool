7113e44824a3e34dba1e12c8c740ca6f06488eab
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-10-24 01:18:19 +0530

    Make class for different entities

--- splitwise.py
+++ splitwise.py
@@ -1,139 +1,183 @@
 # from enum import Enum, unique, auto
 
-
+from datetime import datetime
 # @unique
-# class EType(Enum):
+# class ExpenseType(Enum):
 #     EQUAL = 'equal'
 #     EXACT = 'exact'
 #     PERCENTAGE = 'percentage'
 #     PARTS = 'parts'
 
 
-# class User:
-#     id = 1
-
-#     def __init__(self, name):
-#         self.name = name
-#         self.id = User.id
-#         User.id += 1
+# class InvalidFriendIDsException(Exception):
+#     pass
 
 
-# class Expense:
-#     def __init__(self, payee_id, type, borrower_ids, total_amount, expenses=None):
-#         self.pa
-
-users = {}
-balance = {}
+# class InvalidSharesException(Exception):
+#     pass
 
 
-def add_user(user_id: int, user_name: str):
-    """add user
-
-    Arguments:
-        user_id {int} -- user id
-        user_name {str} -- user name
-
-    Raises:
-        Exception: User already exists
-    """
-    if users.get(user_id):
-        raise Exception('User already exists')
-    users[user_id] = user_name
-    print('New user added')
+# class InvalidAmountException(Exception):
+#     pass
 
 
-def add_balance(from_user_id: int, to_user_id: int, amount: float):
-    """add amount to balance from one user to another
+class Expense:
+    __transactions: dict = {}
+    __balances: dict = {}
+    __data = {}
+    __num: int = 1
 
-    Arguments:
-        from_user_id {int} -- from user id
-        to_user_id {int} -- to user id
-        amount {float} -- amount to add to balance
-    """
-    if not from_user_id in balance:
-        balance[from_user_id] = {}
-    previous_balance = balance.get(from_user_id, {}).get(to_user_id, 0)
-    balance[from_user_id][to_user_id] = previous_balance + amount
+    def __init__(
+            self,
+            title: str,
+            payee_id: int,
+            friend_ids: list,
+            total_amount: float,
+            expense_type: str = 'equal',
+            shares: list = None
+    ):
+        """add expense
+
+        Arguments:
+            title {str} -- expense title
+            payee_id {int} -- payee user id
+            friend_ids {list} -- list of friend user ids
+            total_amount {float} -- total amount
+
+        Keyword Arguments:
+            expense_type {str} -- type of expense (default: {'equal'})
+            shares {list} -- share of each friend in case expense
+                is not equally distributed (default: {None})
+        """
+        # create expense id
+        self.expense_id = Expense.__num
+
+        self.title = title
+        self.payee_id = payee_id
+        self.friend_ids = friend_ids
+        self.total_amount = total_amount
+        self.expense_type = expense_type
+        self.shares = shares
+
+        self.created_at = datetime.now().timestamp
+        # validate
+        self.__validate()
+
+        # calculate amount per friend
+        self.amounts = self.calculate_amounts()
+
+        Expense.__num += 1
+
+        # add transaction
+        self.__add_transaction()
+
+    def __add_transaction(self):
+        # should be atomic
+        for friend_id in self.friend_ids:
+            expenses = self.__transactions.get(friend_id, [])
+            expenses.append(self.expense_id)
+            self.__transactions[friend_id] = expenses
+
+    def __validate(self):
+        # TODO: use custom exceptions
+        if not self.friend_ids:
+            raise Exception('No friends specified')
+        if self.total_amount <= 0:
+            raise Exception('Invalid amount - {}'.format(self.total_amount))
+        for friend_id in self.friend_ids:
+            if User.is_valid(friend_id):
+                continue
+            raise Exception('Invalid friend user id - {}'.format(friend_id))
+
+        if self.expense_type == 'equal':
+            return
+
+        if len(self.friend_ids) != len(self.shares):
+            raise Exception('Number of friends and shares must be equal')
+        if not self.shares:
+            raise Exception('Share of any friend is not given')
+        share_sum = 0
+        for idx, share in enumerate(self.shares):
+            if share < 0:
+                raise Exception(
+                    'Invalid share - {}, for user id - {}'.format(
+                        share, self.friend_ids[idx])
+                )
+            share_sum += share
+
+        if self.expense_type == 'percentage' and share_sum > 100:
+            raise Exception('Invalid total percentage - {}'.format(share_sum))
+
+        if self.expense_type == 'exact' and share_sum != self.total_amount:
+            raise Exception('Sum of shares should be equal to total amount, {} != {}'.format(
+                share_sum, self.total_amount))
+
+        # check valid expense type
+        # raise Exception('Invalid expense type - {}'.format(self.expense_type))
+
+    def calculate_amounts(self):
+        if self.expense_type == 'equal':
+            num_friends = len(self.friend_ids)
+            return [self.total_amount * round(1.0 / num_friends, 2)] * num_friends
+
+        if self.expense_type == 'percentage':
+            return [
+                self.total_amount * round(share / 100, 2) for share in self.shares
+            ]
+
+        if self.expense_type == 'exact':
+            return self.shares
+
+        if self.expense_type == 'parts':
+            share_total = sum(share_total) * 1.0
+            return [
+                self.total_amount * round(share / share_total, 2) for share in self.shares
+            ]
 
 
-def get_amounts(
-        expense_type: str,
-        friend_ids: list,
-        total_amount: float,
-        shares: list
-):
-    if expense_type == 'equal':
-        num_borrower = len(friend_ids)
-        return [total_amount * round(1.0 / num_borrower, 2)] * num_borrower
-    if len(friend_ids) != len(shares):
-        raise Exception('number friends and shares must be equal')
-    if not shares:
-        raise Exception('share of any friend is not given')
+class User:
+    __num = 1
+    __data = {}
 
-    for idx, share in enumerate(shares):
-        if share > 0:
-            continue
-        raise Exception(
-            'Invalid share - {}, for user id - {}'.format(
-                share, friend_ids[idx])
-        )
-    if expense_type == 'percentage':
-        return [
-            total_amount * round(share / 100, 2) for share in shares
-        ]
-    if expense_type == 'exact':
-        return shares
-    if expense_type == 'parts':
-        share_total = sum(share_total) * 1.0
-        return [
-            total_amount * round(share / share_total, 2) for share in shares
-        ]
-    raise Exception('Invalid expense type - {}'.format(expense_type))
+    def __init__(self, name: str, phone_number: str = None, email: str = None):
+        # assign id
+        self.user_id = User.__num
 
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
 
-def add_expense(
-        payer_id: int,
-        friend_ids: list,
-        total_amount: float,
-        expense_type: str = 'equal',
-        shares: list = None
-):
-    """add expense
+        # TODO: sanitize input
 
-    Arguments:
-        payer_id {int} -- payee user id
-        type {str} -- type of expense
-        friend_ids {list} -- list of friend user ids
-        total_amount {float} -- total amount
+        # store user
+        User.__data[self.user_id] = self
+        User.__num += 1
 
-    Keyword Arguments:
-        shares {list} -- share of each friend in case expense
-            is not equally distributed (default: {None})
-    """
-    if not users.get(payer_id):
-        raise Exception('Invalid Payer Id - {}'.format(payer_id))
-    if not friend_ids:
-        raise Exception('No friends specified')
-    if total_amount <= 0:
-        raise Exception('Invalid amount - {}'.format(total_amount))
-    for friend_id in friend_ids:
-        if users.get(friend_id):
-            continue
-        raise Exception('Invalid friend user id - {}'.format(friend_id))
-    amounts = get_amounts(expense_type, friend_ids, total_amount, shares)
-    for friend_id, amount in zip(friend_ids, amounts):
-        add_balance(payer_id, friend_id, -amount)
-        add_balance(friend_id, payer_id, amount)
+    @staticmethod
+    def get_user_by_id(user_id: int):
+        return User.__data.get(user_id)
+
+    @staticmethod
+    def is_valid(user_id: int):
+        return user_id in User.__data
+
+    def add_expense(self, *args, **kwargs):
+        Expense(*args, **kwargs)
+
+    def __repr__(self):
+        return '<User {} : {}>'.format(self.user_id, self.name)
 
 
 # add users
 n = 4
+users = []
 for i in range(1, n+1):
-    add_user(i, 'user {}'.format(i))
+    user = User('username {}'.format(i))
+    users.append(user)
 print(users)
 # add expenses
-add_expense(1, [2], 100)
-add_expense(2, [3], 100)
-print(balance)
-add_expense(3, [2, 4], 1000, 'percentage', [25, 75])
-print(balance)
+# add_expense(1, [2], 100)
+# add_expense(2, [3], 100)
+# print(balance)
+# add_expense(3, [2, 4], 1000, 'percentage', [25, 75])
+# print(balance)
