aa055b3e0561355a2085820639d7015fa551729b
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-10-25 02:07:23 +0530

    test for more attributes in user test

--- tests/test.py
+++ tests/test.py
@@ -4,10 +4,22 @@ from splitfool import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.user = User('test user', '9876543210', 'test@mail.test')
+        self.name = 'test_user'
+        self.phone = '9876543210'
+        self.email = 'test@mail.test'
+        self.user = User(self.name, self.phone, self.email)
 
     def test_id(self):
         self.assertIsInstance(self.user.uid, int)
+
+    def test_name(self):
+        self.assertEqual(self.user.name, self.name)
+
+    def test_phone(self):
+        self.assertEqual(self.user.phone_number, self.phone)
+
+    def test_email(self):
+        self.assertEqual(self.user.email, self.email)
 
 
 if __name__ == '__main__':
