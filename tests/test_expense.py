82a99eae911fcdd969cda0993872d1e462add128
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-10-26 14:50:00 +0530

    update tests

--- tests/test_expense.py
+++ tests/test_expense.py
@@ -1,8 +1,12 @@
 import unittest
-from splitfool import User
+from splitfool import User, ExpenseType
 
 
-class TestExpense(unittest.TestCase):
+class TestEqualExpense(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        User._reset_all()
+
     def setUp(self):
         n = 5
         users = []
@@ -13,9 +17,109 @@ class TestExpense(unittest.TestCase):
         self.user = users[0]
 
     def test_equal_expense(self):
-        self.user.add_expense('uber', self.user.uid, self.friend_ids, 200)
+        self.user.add_expense(
+            'expense equal',
+            self.user.uid,
+            self.friend_ids,
+            200
+        )
+        print(self.user.get_all_data())
+        print(self.user.get_balance())
         self.assertDictEqual(self.user.get_balance(), {
-            2: -66.66, 3: -66.66, 4: -66.66
+            1: -66.66, 2: -66.66, 3: -66.66
+        })
+
+
+class TestExactExpense(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('set up')
+        User._reset_all()
+
+    def setUp(self):
+        n = 5
+        users = []
+        for i in range(1, n+1):
+            user = User('username {}'.format(i))
+            users.append(user)
+        self.friend_ids = list(map(lambda user: user.uid, users[1:-1]))
+        self.user = users[0]
+        print(self.user, self.friend_ids)
+
+    def test_exact_expense(self):
+        shares = [100, 300, 200]
+        self.user.add_expense(
+            'expense exact',
+            self.user.uid,
+            self.friend_ids,
+            600,
+            ExpenseType.EXACT,
+            shares
+        )
+        self.assertDictEqual(self.user.get_balance(), {
+            1: -100, 2: -300, 3: -200
+        })
+
+
+class TestPartsExpense(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('set up')
+        User._reset_all()
+
+    def setUp(self):
+        n = 5
+        users = []
+        for i in range(1, n+1):
+            user = User('username {}'.format(i))
+            users.append(user)
+        self.friend_ids = list(map(lambda user: user.uid, users[1:-1]))
+        self.user = users[0]
+        print(self.user, self.friend_ids)
+
+    def test_parts_expense(self):
+        shares = [50, 100, 80]
+        self.user.add_expense(
+            'expense exact',
+            self.user.uid,
+            self.friend_ids,
+            600,
+            ExpenseType.PARTS,
+            shares
+        )
+        self.assertDictEqual(self.user.get_balance(), {
+            1: -132.0, 2: -258.0, 3: -210.0
+        })
+
+
+class TestPercentageExpense(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('set up')
+        User._reset_all()
+
+    def setUp(self):
+        n = 5
+        users = []
+        for i in range(1, n+1):
+            user = User('username {}'.format(i))
+            users.append(user)
+        self.friend_ids = list(map(lambda user: user.uid, users[1:-1]))
+        self.user = users[0]
+        print(self.user, self.friend_ids)
+
+    def test_percentage_expense(self):
+        shares = [25, 25, 50]
+        self.user.add_expense(
+            'expense exact',
+            self.user.uid,
+            self.friend_ids,
+            600,
+            ExpenseType.PERCENTAGE,
+            shares
+        )
+        self.assertDictEqual(self.user.get_balance(), {
+            1: -150.0, 2: -150.0, 3: -300.0
         })
 
 
